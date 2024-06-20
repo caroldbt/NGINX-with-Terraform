0d98f24856bad39f77740814044ca8335712999b
Carol Burgos <37454302+caroldbt@users.noreply.github.com>
2024-06-20 10:58:07 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -15,20 +15,40 @@ resource "aws_instance" "nginx-server" {
   ami           = "ami-0440d3b780d96b29d"
   instance_type = "t2.micro"
 
+  ## creamos un script de bash, donde instalamos nginx
   user_data = <<-EOF
               #!/bin/bash
               sudo yum install -y nginx
               sudo systemctl enable nginx
               sudo systemctl start nginx
               EOF
-
+  # creamos una varible para hacer referencia a la llave aws_keys_pair de nginx
   key_name               = aws_key_pair.nginx-server-ssh.key_name
   vpc_security_group_ids = [aws_security_group.nginx-server-sg.id]
+
+  # Tags de mi instancia
+  tags = {
+    Name        = "nginx-server"
+    Environment = "test"
+    Owner       = "aqui tu nombre o email"
+    Team        = "DevOps"
+    Project     = "terraform-nginx"
+  }
 }
 
+## keys de nginx
 resource "aws_key_pair" "nginx-server-ssh" {
-  key_name   = "nginx-server-ssh"
+  key_name = "nginx-server-ssh"
+  #subimos nuestra llave en aws 
   public_key = file("nginx-server.key.pub")
+  # Tags de clave ssh
+  tags = {
+    Name        = "nginx-server-ssh"
+    Environment = "test"
+    Owner       = "aqui tu nombre o email"
+    Team        = "DevOps"
+    Project     = "terraform-nginx"
+  }
 }
 
 
@@ -52,11 +72,19 @@ resource "aws_security_group" "nginx-server-sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
-  ## Reglas de salida
+  ## Reglas de salida: aceptando todos los puertos
   egress {
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
+  }
+  # Tags de grupo de seguridad
+  tags = {
+    Name        = "nginx-server-sg"
+    Environment = "test"
+    Owner       = "aqui tu nombre o email"
+    Team        = "DevOps"
+    Project     = "terraform-nginx"
   }
 }
